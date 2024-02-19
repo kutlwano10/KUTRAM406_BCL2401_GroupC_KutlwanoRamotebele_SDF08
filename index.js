70cb763179c3940b0b2ff376c945544d7c34d6ae
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-02-19 10:45:20 +0200

    made the toggle bar respond to the menu

--- index.js
+++ index.js
@@ -2,6 +2,15 @@
 // and be able to see the context inside
 
 const navToggle =document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link'); //will find all elements with this id
+
+
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
 })
